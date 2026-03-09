df14549ed8b6676acd1fe1d6f770a7c2963cd4c0
abdulahad4283-create <abdulahad4283@gmail.com>
2026-03-09 23:56:48 +0500

    Add files via upload

---
+++ LAB 3/LAB WORK/LAB 3 WORK_2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+    {
+int age; //DECLARATION OF VARIABLE
+char hasId; //STORING CHARACTER
+
+cout << "Enter your age:";  //DISPLAY OR PRINT
+cin >> age;   // INOUT FROM USER
+
+cout << "Do you have an ID card? (Y/N):"; 
+cin >> hasId;
+
+if(age >=18 && hasId=='Y'){    //USED IF FOR CONDITION
+	cout << "You can enter" <<endl;  //DISPLAY
+}
+else {    
+	cout<<"Entry not allowed";
+}
+
+return 0;
+}
