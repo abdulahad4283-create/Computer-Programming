d81218d29aada08d80cb6eb6c49f2024f9ef6c49
abdulahad4283-create <abdulahad4283@gmail.com>
2026-03-09 23:55:51 +0500

    Add files via upload

---
+++ LAB 4/LAB TASK/LAB 4 WORK_1.cpp
@@ -0,0 +1,47 @@
+#include <iostream> // Include input-output library
+using namespace std; // Use standard namespace
+
+int main() 
+// Program execution starts here
+	{
+    int day; // Declare variable to store day number
+
+    cout << "Enter day number (1-7): "; // Ask user for input
+    cin >> day; // Take input from user
+
+    switch(day) // Start switch statement
+    {
+        case 1: // If input is 1
+            cout << "Monday"; // Display Monday
+            break; // Exit switch
+
+        case 2:
+            cout << "Tuesday";
+            break;
+
+        case 3:
+            cout << "Wednesday";
+            break;
+
+        case 4:
+            cout << "Thursday";
+            break;
+
+        case 5:
+            cout << "Friday";
+            break;
+
+        case 6:
+            cout << "Saturday";
+            break;
+
+        case 7:
+            cout << "Sunday";
+            break;
+
+        default: // If input is not between 1?7
+            cout << "Invalid day number";
+    }
+
+    return 0; // End program successfully
+	}
---
+++ LAB 4/LAB TASK/LAB 4 WORK_3.cpp
@@ -0,0 +1,48 @@
+#include <iostream> // Include input-output library
+using namespace std; // Use standard namespace
+
+int main() 
+// Main function
+	{
+    char grade; 
+	// Declare variable to store grade
+
+    cout << "Enter grade (A, B, C, D, F): "; // Ask for grade
+    cin >> grade; 
+	// Take input
+
+    switch(grade) 
+	// Start switch statement
+    {
+        case 'A':
+            cout << "Excellent";
+            break;
+
+        case 'B':
+            cout << "Very Good";
+            break;
+
+        case 'C':
+            cout << "Good";
+            break;
+
+        case 'D':
+            cout << "Pass";
+            break;
+		
+        case 'E':
+            cout << "Barely Pass";
+            break;
+		
+        case 'F':
+            cout << "Fail";
+            break;
+
+        default: 
+		// If invalid character entered
+            cout << "Invalid Grade";
+    }
+
+    return 0; 
+	// End program
+	}
