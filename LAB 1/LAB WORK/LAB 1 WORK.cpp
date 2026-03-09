64c558ce3a4c0a0def809ac343fe90b5f7e2d08c
abdulahad4283-create <abdulahad4283@gmail.com>
2026-03-10 00:02:18 +0500

    Add files via upload

---
+++ LAB 1/LAB WORK/LAB 1 WORK.cpp
@@ -0,0 +1,10 @@
+#include <iostream>      //  use for input and output
+
+using namespace std;     // use for writing cout instead of std::cout
+
+int main()               // Main function where program starts
+{
+    cout << "Hello World";   // Prints Hello World on the screen
+    return 0;                // Ends the program
+}
+
