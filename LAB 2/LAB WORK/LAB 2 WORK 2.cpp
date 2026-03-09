7ddce27ed835dbf5d06bb5dd4054bd70075e7060
abdulahad4283-create <abdulahad4283@gmail.com>
2026-03-09 23:22:52 +0500

    Add files via upload

---
+++ LAB 2/LAB WORK/LAB 2 WORK 2.cpp
@@ -0,0 +1,18 @@
+// Name: ABDUL AHAD
+// Department: Electrical Engineering (COMM)
+// Registration No: BF25NWELE00664
+// Section: B
+
+
+
+#include <iostream>
+using namespace std;
+int main()
+    {
+    	float pi; // FLAOT USE KIA HA DECIMAL VALUE K LIYE
+    	pi=3.14; // VALUE DECLARE
+    	cout<<"value of pi =\t"<<pi; // DISPLAY
+
+
+return 0;
+}
---
+++ LAB 2/LAB WORK/LAB 2 WORK 3.cpp
@@ -0,0 +1,23 @@
+// Name: ABDUL AHAD
+// Department: Electrical Engineering (COMM)
+// Registration No: BF25NWELE00664
+// Section: B
+
+
+
+
+
+#include <iostream>
+using namespace std;
+int main()
+    {
+    	double F; // WORDS LIKHNY K LIYE
+    	float C; // DECIMAL VALUE K LIYE 
+    	cout<<"enter temperature in celcius ";  // DISPLAY
+    	cin>>C;
+    F = (C * 9/5) + 32;  // FORMULA CONVERRSION K LIYE
+    	cout<<F <<"  farheight";
+	
+
+return 0;
+}
